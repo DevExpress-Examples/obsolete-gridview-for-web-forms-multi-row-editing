65822706021d6e46be4fa992cffb8a2641cfaba6
Oleg Kozlov <oleg.kozlov@devexpress.com>
2021-02-16 19:28:53 +0300

    Migrate to .NET 4 and v13.1 (WebApp)

---
+++ CS/WebApplication1/Record.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1 {
+    public class Record {
+        int id;
+        string categoryName;
+
+        string description;
+
+        public Record(int id, string categoryName, string description) {
+            this.id = id;
+            this.categoryName = categoryName;
+            this.description = description;
+        }
+        public int Id { get { return this.id; } }
+        public string CategoryName { get { return categoryName; } }
+        public string Description { get { return description; } }
+
+    }
+}
